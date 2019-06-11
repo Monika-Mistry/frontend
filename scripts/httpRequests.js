dfdfe2bce5c3cad317795f493f049b85635efc5b
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-11 17:22:15 +0100

    changes

--- scripts/httpRequests.js
+++ scripts/httpRequests.js
@@ -1,13 +1,33 @@
 function getAnAccountRequest(){
   let req = new XMLHttpRequest();
-
+let id = document.getElementById('accountId').value
   req.onload = function(){
-    let res = document.getElementById('getAnAccount');
-    res.innerHTML = req.responseText;
-
+    if(typeof(document.getElementById('account-result')) != 'undefined' && document.getElementById('account-result') != null){
+      document.getElementById('getAnAccount').removeChild(document.getElementById('account-result'));
+    }
+    let account = document.createElement('p');
+    account.innerHTML = req.responseText;
+    account.id = 'account-result';
+    document.getElementById('getAnAccount').appendChild(account);
   };
 
-  req.open("GET", "http://localhost:8080/AccountSETemplate/api/account/getAnAccount/1");
+  req.open("GET", 'http://localhost:8080/AccountSETemplate/api/account/getAnAccount/'.concat(id));
   req.send();
 
 }
+
+// function getAllAccountsRequest(){
+//   let req = new XMLHttpRequest();
+//
+//   req.onload = function(){
+//     for(let i in req.responseText){
+//       let account = document.createElement('p');
+//       account.innerHTML = i;
+//       account.id = 'account-result';
+//       document.getElementById('getAnAccount').appendChild(account);
+//     }
+//   };
+//
+//   req.open("GET", "http://localhost:8080/AccountSETemplate/api/account/getAllAccounts");
+//   req.send();
+// }
